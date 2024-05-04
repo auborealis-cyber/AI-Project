416e7c81c0a44bc19bb00ba6438bdbe49ac91d34
auborealis-cyber <149447678+auborealis-cyber@users.noreply.github.com>
2024-05-03 21:08:04 -0700

    Added basic js and added typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert("Hello!");
+
+function generateHaiku(event) {
+    event.preventDefault();
+
+  new Typewriter("#haiku", {
+    strings:
+      "Stark, the branches wait, not dead, but in quiet song. Blossom's kiss to come.",
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+    cursor: " ",
+  });
+}
+let poemFormElement = document.querySelector("#haiku-generator");
+poemFormElement.addEventListener("submit", generateHaiku);
